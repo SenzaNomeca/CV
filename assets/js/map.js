c3c0639f560d8dfc03b7d05e8e8e67a41f97fa7e
Roberto Rodriguez <rrodriguezb040@gmail.com>
2024-07-21 12:23:12 -0500

    Ahora solo falta el responsive y la plantilla queda lista

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,8 +1,8 @@
     document.addEventListener("DOMContentLoaded", (event) => {
     setTimeout(() => {
         document.querySelector("#load-iframe-map").innerHTML = `
-            <iframe class="contact__iframe" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" style="border:0;" loading="lazy" referrerpolicy="no-referrer-when-downgrade" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3916.4568353972177!2d-74.80969632411768!3d11.004310489158621!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8ef42da10a56b7d5%3A0xc9964df6a4629125!2sRtno%20Cl.%2078%2C%20Nte.%20Centro%20Historico%2C%20Barranquilla%2C%20Atl%C3%A1ntico!5e0!3m2!1ses!2sco!4v1715815430832!5m2!1ses!2sco"></iframe>
+            <iframe class="contact__iframe" frameborder="0" scrolling="0" marginheight="0" marginwidth="0" loading="lazy" referrerpolicy="no-referrer-when-downgrade" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3916.711467767452!2d-74.80891255965557!3d10.98513748922166!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8ef42d1366b6dd21%3A0x98f91feb2f572f4f!2sCl.%2069d%2C%20El%20Recreo%2C%20Barranquilla%2C%20Atl%C3%A1ntico!5e0!3m2!1ses!2sco!4v1721580976519!5m2!1ses!2sco"  ></iframe>
             `;
-    }, 1000);  
+    }, 500);  /*Esto es milisegundo */
 
     });
